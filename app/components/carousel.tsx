77479d6ec9f3436732554b32e084fa1480180a71
Ayush Mangal <mangal.ayush.4982@gmail.com>
2023-08-27 13:57:55 +0530

    bugs: fixed the isActive property

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -6,6 +6,10 @@ import Slider from 'react-slick';
 import carouselData from '../data/carouselData.json';
 import Image from 'next/image';
 
+interface WrapProps {
+  isactive: string
+}
+
 const ImgSlider: React.FC = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
@@ -16,65 +20,69 @@ const ImgSlider: React.FC = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    beforeChange: (current: any, next: React.SetStateAction<number>) => setActiveIndex(next),
-    afterChange: () => {},
+    beforeChange: (current: number, next: number) => setActiveIndex(next),
   };
 
   return (
     <div className='section'>
       <div className='car_section'>
-      <div className='carousel_section'>
-      <Carousel {...settings}>
-        {carouselData.map((imageUrl, index) => (
-          <Wrap key={index} isActive = {index === activeIndex}>
-            <a>
-              <div className='image-container'>
-                <Image src={imageUrl} alt={`Slide ${index + 1}`} layout='fill' objectFit='cover' />
-              </div>
-            </a>
-          </Wrap>
-        ))}
-      </Carousel>
-    </div>
-    </div>
+        <div className='carousel_section'>
+          <Carousel {...settings}>
+            {carouselData.images.map((imageUrl, index) => (
+              <Wrap key={index} isactive={index === activeIndex ? "true" : ""}>
+                <a>
+                  <div className='image-container'>
+                    <Image src={imageUrl} alt={`Slide ${index + 1}`} layout='fill' objectFit='cover' />
+                  </div>
+                </a>
+              </Wrap>
+            ))}
+          </Carousel>
+        </div>
+      </div>
     </div>
   );
 };
 
 const Carousel = styled(Slider)`
-    margin-top: 20px;
-    & > button {
-        opacity: 0;
-        height: 100%;
-        width: 5vw;
-        z-index: 1 ;
+  margin-top: 20px;
+  & > button {
+    opacity: 0;
+    height: 100%;
+    width: 5vw;
+    z-index: 1;
 
-        &:hover {
-            opacity:1;
-            transition: opacity 0.2s ease 0s; 
-        }
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2s ease 0s;
     }
-    
-    ul li button {
-        &:before {
-            font-size:10px;
-            color:rgb(150,158,171);
-        }
+  }
+
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
     }
-    li.slick-active button:before {
-        color:white;
-    }
-    .slick-list{
-        overflow:initial;
-    }
-    .slick-prev{
-        left:-100px;
-    }
-    .slick-next{
-        right:-100px;
-    }
-`
-const Wrap = styled.div<{isActive:boolean}>`
+  }
+
+  li.slick-active button:before {
+    color: white;
+  }
+
+  .slick-list {
+    overflow: initial;
+  }
+
+  .slick-prev {
+    left: -100px;
+  }
+
+  .slick-next {
+    right: -100px;
+  }
+`;
+
+const Wrap = styled.div<WrapProps>`
   border-radius: 6px;
   cursor: pointer;
   position: relative;
@@ -91,11 +99,11 @@ const Wrap = styled.div<{isActive:boolean}>`
     position: relative;
     width: 100%;
     border-radius: 10px;
-    height: 60vh; 
-    transform: scale(${props => (props.isActive ? '1.1' : '1')});
+    height: 60vh;
+    transform: scale(${props => (props.isactive ? '1.1' : '1')});
     transition: transform 0.3s ease;
     ${props =>
-      props.isActive &&
+      props.isactive &&
       `
       box-shadow: 0px 10px 20px rgba(0, 0, 0, 1);
       z-index:2;
@@ -106,7 +114,6 @@ const Wrap = styled.div<{isActive:boolean}>`
     padding: 0;
     transition: 100ms;
   }
-
-`;
+`
 
 export default ImgSlider;
