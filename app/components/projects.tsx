bed636121f0b8196c012acc680bfef5a3d1d618c
Ayush Mangal <mangal.ayush.4982@gmail.com>
2023-08-29 22:27:26 +0530

    fix: tooltip added to projects

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,51 +1,63 @@
-import React from 'react'
-import projectsData from '../data/projectsData.json'
-import Link from 'next/link'
-import { FaGithub, FaYoutube, FaLink } from 'react-icons/fa'
+import React from "react";
+import projectsData from "../data/projectsData.json";
+import Link from "next/link";
+import {
+  FaGithub,
+  FaYoutube,
+  FaGlobe,
+} from "react-icons/fa";
+import ToolTip from "./toolTip";
 
 const Projects = () => {
   return (
-    <div className='section'>
-      <h1 className='heading text-lg '>
-        {projectsData.heading}
-      </h1>
-      <div className='project_container'>
-            {projectsData.data.map(project => (
-                <div className='project_card' key={project.id}>
-                    <h1 className='project_name pb-2'>
-                        {project.name}
-                    </h1>
-                    <hr className='pt-2'/>
-                    <div className='description_box'>
-                        <div>
-                        <p className='project_description p-5 flex items-center'>
-                            {project.description}
-                        </p>
-
-                        </div>
-                        <div className='links'>
-                            <div>
-                            {project.videoUrl && (
-                                <Link className='link' href={project.videoUrl}><FaYoutube/></Link>
-                            )}
-                            </div>
-                            <div>
-                            {project.deployedUrl && (
-                                <Link className='link' href={project.deployedUrl}><FaLink/></Link>
-                            )}
-                            </div>
-                            <div>
-                            {project.githubUrl && (
-                                <Link className='link' href={project.githubUrl}><FaGithub/></Link>
-                            )}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            ))}
+    <div className="section">
+      <h1 className="heading text-lg ">{projectsData.heading}</h1>
+      <div className="project_container">
+        {projectsData.data.map((project) => (
+          <div className="project_card" key={project.id}>
+            <h1 className="project_name pb-2">{project.name}</h1>
+            <hr className="pt-2" />
+            <div className="description_box">
+              <div>
+                <p className="project_description p-5 flex items-center">
+                  {project.description}
+                </p>
+              </div>
+              <div className="links">
+                <ToolTip tooltip="Youtube">
+                  <div>
+                    {project.videoUrl && (
+                      <Link className="link" target="_blank" href={project.videoUrl}>
+                        <FaYoutube />
+                      </Link>
+                    )}
+                  </div>
+                </ToolTip>
+                <ToolTip tooltip="Live Site">
+                  <div>
+                    {project.deployedUrl && (
+                      <Link className="link" target="_blank" href={project.deployedUrl}>
+                        <FaGlobe />
+                      </Link>
+                    )}
+                  </div>
+                </ToolTip>
+                <ToolTip tooltip="Github">
+                  <div>
+                    {project.githubUrl && (
+                      <Link className="link" target="_blank" href={project.githubUrl}>
+                        <FaGithub />
+                      </Link>
+                    )}
+                  </div>
+                </ToolTip>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
--- app/components/toolTip.tsx
+++ app/components/toolTip.tsx
@@ -1,27 +1,28 @@
-import React, { FC, ReactNode, useRef } from 'react'
+import React, { FC, ReactNode, useRef } from 'react';
 
 interface Props {
-    children : ReactNode
-    tooltip : string
+  children: ReactNode;
+  tooltip: string;
 }
 
-const ToolTip:FC<Props> = ({children, tooltip}:any): JSX.Element => {
-    const tooltipRef = useRef<HTMLSpanElement>(null);
-    const container = useRef<HTMLDivElement>(null);
+const ToolTip: FC<Props> = ({ children, tooltip }: Props): JSX.Element => {
+  const tooltipRef = useRef<HTMLSpanElement>(null);
+  const container = useRef<HTMLDivElement>(null);
+
   return (
-    <div ref={container} onMouseEnter={({clientX}) => {
-        if(!tooltipRef.current || !container.current) return
-        const {left} = container.current.getBoundingClientRect()
-        tooltipRef.current.style.left = clientX - left + 'px'
-    }} 
-        className='tooltip_container group'
+    <div
+      ref={container}
+      className='tooltip_container group relative'
     >
       {children}
-      <span ref = {tooltipRef} className='invisible group-hover:visible opacity-0 group-hover:opacity-100 transition p-1 rounded absolute top-full mt-2 whitespace-nowrap'>
+      <span
+        ref={tooltipRef}
+        className='invisible group-hover:visible opacity-0 group-hover:opacity-100 transition p-1 rounded absolute top-full mt-2 whitespace-nowrap transform -translate-y-full md:-translate-x-1/2 left-1/2'
+      >
         {tooltip}
       </span>
     </div>
-  )
-}
+  );
+};
 
-export default ToolTip
+export default ToolTip;
