212677b34dcd9b1c2a5c63a4f68b8e2333ad3d45
Ayush Mangal <mangal.ayush.4982@gmail.com>
2023-09-13 16:13:28 +0530

    feat: added customizable icon and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import websiteData from './data/websiteData.json'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <title>{websiteData.title}</title>
+      <meta name='description' content={websiteData.description}/>
+      <link href={websiteData.icon} rel='icon'/>
       <body className={inter.className}>{children}</body>
     </html>
   )
