ac5890ef0c76f03285ba64e5fd8df7fbaea3e79f
Ayush Mangal <mangal.ayush.4982@gmail.com>
2023-09-13 16:52:14 +0530

    fix: workEx issue code break

--- app/components/workEx.tsx
+++ app/components/workEx.tsx
@@ -1,56 +1,16 @@
+"use client";
+
 import React, { useState } from "react";
 import FilterBar from "./filterBar";
 import workExData from "../data/workExData.json";
 
-// Define a custom type for your JSON data
-interface WorkExData {
-  heading: string;
-  data: {
-    organization: string;
-    duration: string;
-    skills: string[];
-    role: string;
-    description: string[];
-    type: string[];
-  }[];
-  links: {
-    [key: string]: string;
-  };
-}
-
 const WorkEx = () => {
-  const types = ["All", ...new Set(workExData.data.flatMap((item) => item.type))];
-  const [selectedType, setSelectedType] = useState<string>("All");
+  const types = ["All", ...new Set(workExData.data.flatMap((item) => item.type))]; 
+  const [selectedType, setSelectedType] = useState<string>("All"); 
 
-  const filteredData = selectedType === "All" ? workExData.data : workExData.data.filter((item) => item.type.includes(selectedType));
-
-  // Function to add links to words based on the "links" section in the JSON data
-  const addLinksToHighlightedWords = (content: any) => {
-    return content.split(/<highlight>(.*?)<\/highlight>/g).map((part:any, index:any) => {
-      if (index % 2 === 1) {
-        // Odd-indexed parts are highlighted and should be linked if found in "links"
-        const link = (workExData.links as WorkExData["links"])[part];
-        if (link) {
-          return (
-            <a key={index} href={link}>
-              {part}
-            </a>
-          );
-        } else {
-          // If no link is found, display the highlighted part without a link
-          return (
-            <span key={index} className="highlight">
-              {part}
-            </span>
-          );
-        }
-      } else {
-        // Even-indexed parts are not highlighted and should be displayed as is
-        return part;
-      }
-    });
-  };
-
+  const filteredData = selectedType === "All" 
+    ? workExData.data
+    : workExData.data.filter((item) => item.type.includes(selectedType));
 
   return (
     <div className="section">
@@ -64,24 +24,25 @@ const WorkEx = () => {
         {filteredData.map((item, index) => (
           <div key={index} className="experience_content">
             <div className="title_box pb-4">
-              <div className="experience_name">{item.organization}</div>
-              <div className="duration">{item.duration}</div>
+              <div className="experience_name"> {item.organization}</div>
+              <div className="duration ">{item.duration}</div>
             </div>
             <hr />
             <div className="role">{item.role}</div>
             <div className="work_skill">
-              {item.skills.map((skill, index) => (
-                <div className="work_skill_name" key={index}>
-                  {skill}
-                </div>
-              ))}
+            {item.skills.map((skill,index) => (
+                <div className="work_skill_name" key= {index}>{skill}
+                    </div>
+            ))}
             </div>
             <ul className="work_description">
               {item.description.map((bullet, index) => (
                 <li
                   key={index}
                   dangerouslySetInnerHTML={{
-                    __html: addLinksToHighlightedWords(bullet),
+                    __html: bullet
+                      .replace(/<highlight>/g, '<span class="highlight">')
+                      .replace(/<\/highlight>/g, "</span>"),
                   }}
                 />
               ))}
